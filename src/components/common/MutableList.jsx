553922e376f355f6af5ec25e134e3d188721df7e
Captain_Mitchell <captain.mitchell.ca@gmail.com>
2020-12-18 01:56:04 +0300

    Implement search for mutable list

--- src/components/common/MutableList.jsx
+++ src/components/common/MutableList.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import axios from 'axios';
+
+import Input from 'components/common/Input';
 
 import useModal from 'hooks/useModal';
 
 const MutableList = ({ items, editModalName, createModalName, collectionName, refetch }) => {
   const { open: openCreate } = useModal(createModalName);
+
+  const [search, setSearch] = useState('');
+
+  const filteredItems = useMemo(() => items?.filter(({ label }) => new RegExp(search, 'gi').test(label)), [
+    search,
+    items,
+  ]);
 
   const onDelete = (value, name) => {
     const sure = confirm(`Вы действительно хотите удалить элемент ${name}?`);
@@ -19,10 +28,15 @@ const MutableList = ({ items, editModalName, createModalName, collectionName, re
 
   return (
     <div className="mutable-list">
-      <button onClick={openCreate} className="create-item">
-        Добавить элемент
-      </button>
-      {items?.map(({ label, value }) => (
+      <div className="mutable-list-header">
+        <div className="search">
+          <Input label="Поиск" value={search} onChange={({ value }) => setSearch(value)} />
+        </div>
+        <button onClick={openCreate} className="create-item">
+          Добавить элемент
+        </button>
+      </div>
+      {(search ? filteredItems : items)?.map(({ label, value }) => (
         <div className="list-item" key={`${label}-${value}`}>
           <div className="item-name">{label}</div>
           <div className="item-actions">
